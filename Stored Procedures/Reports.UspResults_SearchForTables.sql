4e228758ee602ae4a57b8e9432c74d49962e3dcb
Beardy <chris.johnson85@gmail.com>
2016-01-21 13:33:16 +0000

    Changed script to iterate through all databases and return db name as well

--- Stored Procedures/Reports.UspResults_SearchForTables.sql
+++ Stored Procedures/Reports.UspResults_SearchForTables.sql
@@ -1,8 +1,9 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-Create Proc [Reports].[UspResults_SearchForTables]
+CREATE Proc [Reports].[UspResults_SearchForTables]
     (
       @ColumnSearch Varchar(150)
     , @TableSearch Varchar(150)
@@ -19,56 +20,58 @@ The purpose of this stored procedure is to search for columns in tables returnin
     Set @TableSearch = '%' + Coalesce(@TableSearch , '') + '%';
     Set @SchemaSearch = '%' + Coalesce(@SchemaSearch , '') + '%';
 
+    Declare @SQLScript Varchar(2000);
+
     Create Table [#ResultSets]
         (
           [SchemaName] Varchar(300)
         , [TableName] Varchar(300)
         , [ColumnName] Varchar(150)
+        , [DatabaseName] Varchar(300)
         , [AllColumns] Varchar(Max) --list of all columns in table
         , [DistinctScript] Varchar(Max) --script to get distinct values of returned column
         , [Top10Script] Varchar(Max) --script to get top 10 with all fields from table
         );
 
-    Insert  [#ResultSets]
-            ( [SchemaName]
-            , [TableName]
-            , [ColumnName]
-            , [AllColumns]
-            , [DistinctScript]
-            , [Top10Script]
-	        )
-            Select  [SchemaName] = QuoteName([S].[name]) --The QuoteName function returns the value ready for SQL e.g. [Field !324342]
+    Set @SQLScript = 'Use [?]; Insert  [#ResultSets]
+            ( [SchemaName], [TableName], [ColumnName], [DatabaseName], [AllColumns], [DistinctScript], [Top10Script])
+            Select  [SchemaName] = QuoteName([S].[name])
                   , [TableName] = QuoteName([T].[name])
                   , [ColumnName] = QuoteName([C].[name])
+				  , [DatabaseName] = QuoteName(db_name())
                   , [AllColumns] = Stuff(( Select Distinct
-                                                    ', '
+                                                    '', ''
                                                     + QuoteName([C2].[name])
                                            From     [sys].[columns] [C2]
                                            Where    [C2].[object_id] = [C].[object_id]
                                          For
-                                           Xml Path('')
-                                         ) , 1 , 1 , '')
-                  , [DistinctScript] = 'Select distinct '
-                    + QuoteName([C].[name]) + ' from ' + QuoteName([S].[name])
-                    + '.' + QuoteName([T].[name])
-                  , [Top10Script] = 'Select Top 10 '
+                                           Xml Path('''')
+                                         ) , 1 , 1 , '''')
+                  , [DistinctScript] = ''Select distinct ''
+                    + QuoteName([C].[name]) + '' from '' + QuoteName([S].[name])
+                    + ''.'' + QuoteName([T].[name])
+                  , [Top10Script] = ''Select Top 10 ''
                     + Stuff(( Select Distinct
-                                        ', ' + QuoteName([C2].[name])
+                                        '', '' + QuoteName([C2].[name])
                               From      [sys].[columns] [C2]
                               Where     [C2].[object_id] = [C].[object_id]
                             For
-                              Xml Path('')
-                            ) , 1 , 1 , '') + ' from ' + [S].[name] + '.'
+                              Xml Path('''')
+                            ) , 1 , 1 , '''') + '' from '' + [S].[name] + ''.''
                     + [T].[name]
             From    [sys].[columns] [C]
                     Inner Join [sys].[tables] [T] On [T].[object_id] = [C].[object_id]
                     Inner Join [sys].[schemas] [S] On [S].[schema_id] = [T].[schema_id]
-            Where   Lower([C].[name]) Like Lower(@ColumnSearch)
-                    And Lower([T].[name]) Like Lower(@TableSearch)
-                    And Lower([S].[name]) Like Lower(@SchemaSearch); --cast as lower to cater for case sensitive db's
+            Where   Lower([C].[name]) Like Lower('''+@ColumnSearch+''')
+                    And Lower([T].[name]) Like Lower('''+@TableSearch+''')
+                    And Lower([S].[name]) Like Lower('''+@SchemaSearch+'''); ';
  
   
-    Select  [RS].[SchemaName]
+    Exec [Process].[ExecForEachDB] @cmd = @SQLScript;
+  
+
+    Select  [RS].[DatabaseName]
+		  , [RS].[SchemaName]
           , [RS].[TableName]
           , [RS].[ColumnName]
           , [RS].[AllColumns]
