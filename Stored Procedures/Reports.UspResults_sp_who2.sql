dcc210940c1c9e1f06ab8da30ab22870cb09be51
Beardy <chris.johnson85@gmail.com>
2016-01-21 15:09:53 +0000

    defined field list to be inserted into

--- Stored Procedures/Reports.UspResults_sp_who2.sql
+++ Stored Procedures/Reports.UspResults_sp_who2.sql
@@ -1,53 +1,62 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-Create Proc [Reports].[UspResults_sp_who2]
- /*
-Stored Procedure created by Chris Johnson
-21st January 2016
-
-The purpose of this stored procedure is to provide the results of sp_who2 in a better format, returning distinct fields and ordered by useful info
-*/
+CREATE Proc [Reports].[UspResults_sp_who2]
 As
-    Begin 
-        Declare @Results Table
-            (
-              [SPID] Int
-            , [Status] Varchar(Max)
-            , [LOGIN] Varchar(Max)
-            , [HostName] Varchar(Max)
-            , [BlkBy] Varchar(Max)
-            , [DBName] Varchar(Max)
-            , [Command] Varchar(Max)
-            , [CPUTime] Int
-            , [DiskIO] Int
-            , [LastBatch] Varchar(Max)
-            , [ProgramName] Varchar(Max)
-            , [SPID_1] Int
-            , [REQUESTID] Int
-            );
+	Begin 
+		Declare @Results Table
+			(
+				[SPID] Int
+			, [Status] Varchar(Max)
+			, [LOGIN] Varchar(Max)
+			, [HostName] Varchar(Max)
+			, [BlkBy] Varchar(Max)
+			, [DBName] Varchar(Max)
+			, [Command] Varchar(Max)
+			, [CPUTime] Int
+			, [DiskIO] Int
+			, [LastBatch] Varchar(Max)
+			, [ProgramName] Varchar(Max)
+			, [SPID_1] Int
+			, [REQUESTID] Int
+			);
 
-        Insert  Into @Results
-                Exec [sys].[sp_who2];
+		Insert  Into @Results
+				( [SPID]
+				, [Status]
+				, [LOGIN]
+				, [HostName]
+				, [BlkBy]
+				, [DBName]
+				, [Command]
+				, [CPUTime]
+				, [DiskIO]
+				, [LastBatch]
+				, [ProgramName]
+				, [SPID_1]
+				, [REQUESTID]
+				)
+				Exec [sys].[sp_who2];
 
 		--Only show SPID once, show blocked processes first, followed by CPU time and DISK IO hoggers
-        Select  [SPID]
-              , [Status]
-              , [LOGIN]
-              , [HostName]
-              , [BlkBy]
-              , [DBName]
-              , [Command]
-              , [CPUTime]
-              , [DiskIO]
-              , [LastBatch]
-              , [ProgramName]
-              , [REQUESTID]
-        From    @Results
-        Order By [BlkBy] Asc
-              , [CPUTime] Desc
-              , [DiskIO] Desc
-              , [SPID] Asc;
-    End;
+		Select  [SPID]
+				, [Status]
+				, [LOGIN]
+				, [HostName]
+				, [BlkBy]
+				, [DBName]
+				, [Command]
+				, [CPUTime]
+				, [DiskIO]
+				, [LastBatch]
+				, [ProgramName]
+				, [REQUESTID]
+		From    @Results
+		Order By [BlkBy] Asc
+				, [CPUTime] Desc
+				, [DiskIO] Desc
+				, [SPID] Asc;
+	End;
 GO
