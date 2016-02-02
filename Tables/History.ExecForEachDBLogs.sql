e705fbaab1589999b78996fae5d351df41c33f49
Beardy <chris.johnson85@gmail.com>
2016-02-02 10:08:32 +0000

    Expanded Cmd column for larger queries

--- Tables/History.ExecForEachDBLogs.sql
+++ Tables/History.ExecForEachDBLogs.sql
@@ -2,6 +2,6 @@ CREATE TABLE [History].[ExecForEachDBLogs]
 (
 [LogID] [bigint] NOT NULL IDENTITY(1, 1),
 [LogTime] [datetime2] NULL CONSTRAINT [DF__ExecForEa__LogTi__117F9D94] DEFAULT (getdate()),
-[Cmd] [nvarchar] (2000) COLLATE Latin1_General_BIN NULL
-) ON [PRIMARY]
+[Cmd] [varchar] (max) COLLATE Latin1_General_BIN NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
