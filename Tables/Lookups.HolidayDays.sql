61a367eeba822ac5d785d4672911b31491892384
Beardy <chris.johnson85@gmail.com>
2016-02-08 11:40:48 +0000

    Added key to table

--- Tables/Lookups.HolidayDays.sql
+++ Tables/Lookups.HolidayDays.sql
@@ -1,9 +1,11 @@
 CREATE TABLE [Lookups].[HolidayDays]
 (
-[Country] [varchar] (150) COLLATE Latin1_General_BIN NULL,
+[Country] [varchar] (150) COLLATE Latin1_General_BIN NOT NULL,
 [HolidayDesc] [varchar] (200) COLLATE Latin1_General_BIN NULL,
-[HolidayDate] [date] NULL
+[HolidayDate] [date] NOT NULL
 ) ON [PRIMARY]
-GO
+ALTER TABLE [Lookups].[HolidayDays] ADD 
+CONSTRAINT [HD_PrimKey] PRIMARY KEY CLUSTERED  ([Country], [HolidayDate]) ON [PRIMARY]
 CREATE NONCLUSTERED INDEX [HolidayDays_Date] ON [Lookups].[HolidayDays] ([HolidayDate], [Country]) ON [PRIMARY]
+
 GO
