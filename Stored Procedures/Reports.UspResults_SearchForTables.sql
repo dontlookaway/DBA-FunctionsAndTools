e90261c95bfe6164690c50cca66adbfd81fc4c81
Beardy <chris.johnson85@gmail.com>
2016-03-29 15:11:12 +0100

    added database search

--- Stored Procedures/Reports.UspResults_SearchForTables.sql
+++ Stored Procedures/Reports.UspResults_SearchForTables.sql
@@ -8,6 +8,7 @@ CREATE Proc [Reports].[UspResults_SearchForTables]
       @ColumnSearch Varchar(150)
     , @TableSearch Varchar(150)
     , @SchemaSearch Varchar(150)
+    , @DBSearch Varchar(150)
     )
 As /*
 Stored Procedure created by Chris Johnson
@@ -19,6 +20,7 @@ The purpose of this stored procedure is to search for columns in tables returnin
     Set @ColumnSearch = '%' + Coalesce(@ColumnSearch , '') + '%';
     Set @TableSearch = '%' + Coalesce(@TableSearch , '') + '%';
     Set @SchemaSearch = '%' + Coalesce(@SchemaSearch , '') + '%';
+    Set @DBSearch = '%' + Coalesce(@DBSearch , '') + '%';
 
     Declare @SQLScript Varchar(2000);
 
@@ -33,45 +35,48 @@ The purpose of this stored procedure is to search for columns in tables returnin
         , [Top10Script] Varchar(Max) --script to get top 10 with all fields from table
         );
 
-    Set @SQLScript = 'Use [?]; Insert  [#ResultSets]
-            ( [SchemaName], [TableName], [ColumnName], [DatabaseName], [AllColumns], [DistinctScript], [Top10Script])
-            Select  [SchemaName] = QuoteName([S].[name])
-                  , [TableName] = QuoteName([T].[name])
-                  , [ColumnName] = QuoteName([C].[name])
-				  , [DatabaseName] = QuoteName(db_name())
-                  , [AllColumns] = Stuff(( Select Distinct
-                                                    '', ''
-                                                    + QuoteName([C2].[name])
-                                           From     [sys].[columns] [C2]
-                                           Where    [C2].[object_id] = [C].[object_id]
-                                         For
-                                           Xml Path('''')
-                                         ) , 1 , 1 , '''')
-                  , [DistinctScript] = ''Select distinct ''
-                    + QuoteName([C].[name]) + '' from '' + QuoteName([S].[name])
-                    + ''.'' + QuoteName([T].[name])
-                  , [Top10Script] = ''Select Top 10 ''
-                    + Stuff(( Select Distinct
-                                        '', '' + QuoteName([C2].[name])
-                              From      [sys].[columns] [C2]
-                              Where     [C2].[object_id] = [C].[object_id]
-                            For
-                              Xml Path('''')
-                            ) , 1 , 1 , '''') + '' from '' + [S].[name] + ''.''
-                    + [T].[name]
-            From    [sys].[columns] [C]
-                    Inner Join [sys].[tables] [T] On [T].[object_id] = [C].[object_id]
-                    Inner Join [sys].[schemas] [S] On [S].[schema_id] = [T].[schema_id]
-            Where   Lower([C].[name]) Like Lower('''+@ColumnSearch+''')
-                    And Lower([T].[name]) Like Lower('''+@TableSearch+''')
-                    And Lower([S].[name]) Like Lower('''+@SchemaSearch+'''); ';
+    Set @SQLScript = 'Use [?]; 
+	If  Lower(Db_Name()) Like Lower(''' +@DBSearch+ ''')
+	Begin
+		Insert  [#ResultSets] ( [SchemaName], [TableName], [ColumnName], [DatabaseName], [AllColumns], [DistinctScript], [Top10Script])
+		Select  [SchemaName] = QuoteName([S].[name])
+				, [TableName] = QuoteName([T].[name])
+				, [ColumnName] = QuoteName([C].[name])
+				, [DatabaseName] = QuoteName(db_name())
+				, [AllColumns] = Stuff(( Select Distinct
+												'', ''
+												+ QuoteName([C2].[name])
+										From     [sys].[columns] [C2]
+										Where    [C2].[object_id] = [C].[object_id]
+										For
+										Xml Path('''')
+										) , 1 , 1 , '''')
+				, [DistinctScript] = ''Select distinct ''
+				+ QuoteName([C].[name]) + '' from '' + QuoteName([S].[name])
+				+ ''.'' + QuoteName([T].[name])
+				, [Top10Script] = ''Select Top 10 ''
+				+ Stuff(( Select Distinct
+									'', '' + QuoteName([C2].[name])
+							From      [sys].[columns] [C2]
+							Where     [C2].[object_id] = [C].[object_id]
+						For
+							Xml Path('''')
+						) , 1 , 1 , '''') + '' from '' + [S].[name] + ''.''
+				+ [T].[name]
+		From    [sys].[columns] [C]
+				Inner Join [sys].[tables] [T] On [T].[object_id] = [C].[object_id]
+				Inner Join [sys].[schemas] [S] On [S].[schema_id] = [T].[schema_id]
+		Where   Lower([C].[name]) Like Lower(''' +@ColumnSearch+ ''')
+				And Lower([T].[name]) Like Lower(''' +@TableSearch+ ''')
+				And Lower([S].[name]) Like Lower(''' +@SchemaSearch+ '''); 
+	end';
  
   
     Exec [Process].[ExecForEachDB] @cmd = @SQLScript;
   
 
     Select  [RS].[DatabaseName]
-		  , [RS].[SchemaName]
+          , [RS].[SchemaName]
           , [RS].[TableName]
           , [RS].[ColumnName]
           , [RS].[AllColumns]
