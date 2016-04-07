aef88413356ebebd0d7f4c50aa618600625e1e1f
Beardy <chris.johnson85@gmail.com>
2016-04-07 15:47:28 +0100

    Revert "removed due to Redgate error"
    This reverts commit 0eccf5267f4be55c69b8cc795f003143c8a32da8.

---
+++ Tables/dbo.GetActiveDirectoryGroupMembers.sql
@@ -0,0 +1,16 @@
+CREATE TABLE [dbo].[GetActiveDirectoryGroupMembers]
+(
+[ID] [int] NOT NULL IDENTITY(1, 1),
+[GroupCanonicalName] [varchar] (128) COLLATE Latin1_General_BIN NOT NULL,
+[GroupSamAccountName] [varchar] (128) COLLATE Latin1_General_BIN NOT NULL,
+[DomainName] AS (left([GroupCanonicalName],charindex('/',[GroupCanonicalName])-(1))),
+[SamAccountName] [varchar] (128) COLLATE Latin1_General_BIN NOT NULL,
+[ObjectClass] [varchar] (128) COLLATE Latin1_General_BIN NOT NULL,
+[DWCreatedDate] [datetime] NULL CONSTRAINT [DF__GetActiveDirectoryGroupMembers__DWCreatedDate] DEFAULT (getdate()),
+[DWUpdatedDate] [datetime] NULL,
+[DWIsDeleted] [bit] NULL CONSTRAINT [DF__GetActiveDirectoryGroupMembers__DWIsDeleted] DEFAULT ((0)),
+[DWDeletedDate] [datetime] NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[GetActiveDirectoryGroupMembers] ADD CONSTRAINT [UQ_GetActiveDirectoryGroupMembers_GroupUserObject] UNIQUE CLUSTERED  ([GroupCanonicalName], [GroupSamAccountName], [SamAccountName], [ObjectClass]) ON [PRIMARY]
+GO
