95aa6529eb2999f9f4e58bd973f0a6e4ffe454ea
Beardy <chris.johnson85@gmail.com>
2016-06-30 15:07:56 +0100

    formatted code, catered for case sensitive dbs, catered for blank values, removed delete statement and replaced with selects

--- Stored Procedures/Reports.UspResults_SearchForText.sql
+++ Stored Procedures/Reports.UspResults_SearchForText.sql
@@ -1,4 +1,3 @@
-
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -18,11 +17,14 @@ As
         Set NoCount On;
         Declare @StartDate DateTime2 = GetDate();
         Declare @SQLScript NVarchar(Max);
-        Set @TextToSearch = '%' + Coalesce(@TextToSearch , '') + '%';
-        Set @DBToSearch = '%' + Coalesce(@DBToSearch , '') + '%';
-        Set @SchemaToSearch = '%' + Coalesce(@SchemaToSearch , '') + '%';
-        Set @ObjectName = '%' + Coalesce(@ObjectName , '') + '%';
-        Set @TableView = Coalesce(@TableView , 'Both');
+        Set @TextToSearch = '%' + Coalesce(Lower(@TextToSearch) , '') + '%';
+        Set @DBToSearch = '%' + Coalesce(Lower(@DBToSearch) , '') + '%';
+        Set @SchemaToSearch = '%' + Coalesce(Lower(@SchemaToSearch) , '')
+            + '%';
+        Set @ObjectName = '%' + Coalesce(Lower(@ObjectName) , '') + '%';
+        Set @TableView = Coalesce(Case When @TableView = '' Then Null
+                                       Else Lower(@TableView)
+                                  End , 'both');
 
         Create Table [#ObjectList]
             (
@@ -32,9 +34,10 @@ As
             , [SchemaName] sysname
             , [DBName] Varchar(255) Not Null
             , [ObjectDefinition] NVarchar(Max)
-			, Constraint [PK_ObjList_ObjectID_DBName] Primary Key ([object_id],[DBName])
+            , Constraint [PK_ObjList_ObjectID_DBName] Primary Key
+                ( [object_id] , [DBName] )
             );
-        --Alter Table [#ObjectList] Add Constraint [PK_ObjList_ObjectID_DBName] Primary Key ([object_id],[DBName]);
+
         Create Table [#ColumnList]
             (
               [object_id] Int Not Null
@@ -44,64 +47,76 @@ As
             , [max_length] Smallint
             , [DBName] Varchar(255) Not Null
             , [ExecID] Int Identity(1 , 1)
-			, Constraint [PL_ColList_ObjectID_ColID_DBName] Primary Key ([object_id],[ColumnID],[DBName])
+            , Constraint [PL_ColList_ObjectID_ColID_DBName] Primary Key
+                ( [object_id] , [ColumnID] , [DBName] )
             );
-        --Alter Table [#ColumnList] Add Constraint [PL_ColList_ObjectID_ColID_DBName] Primary Key ([object_id],[ColumnID],[DBName]);
 
 --Get list of table or views
-        If @TableView In ( 'Table' , 'Both' )
+        If @TableView In ( 'table' , 'both' )
             Begin
                 Set @SQLScript = 'Use [?]; 
 			Declare @DB Varchar(250)= Db_Name();
-			Insert [#ObjectList]
-					( [object_id]
-					, [ObjectName]
-					, [ObjectType]
-					, [SchemaName]
-					, [DBName]
-					, [ObjectDefinition]
-					)
-			Select  [T].[object_id]
-				  , ObjectName = [T].[name]
-				  , ObjectType = ''Table''
-				  , SchemaName = [S].[name]
-				  , DBName = @DB
-				  , [ObjectDefinition]=null --Object_Definition([T].[object_id])
-			From    [sys].[tables] As [T]
-					Left Join [sys].[schemas] As [S] On [S].[schema_id] = [T].[schema_id];';
+			if @DB not in(''tempdb'',''master'',''model'',''msdb'') and lower(@DB) like '''
+                    + @DBToSearch
+                    + '''
+			begin
+				Insert [#ObjectList]
+						( [object_id]
+						, [ObjectName]
+						, [ObjectType]
+						, [SchemaName]
+						, [DBName]
+						, [ObjectDefinition]
+						)
+				Select  [T].[object_id]
+					  , ObjectName = [T].[name]
+					  , ObjectType = ''Table''
+					  , SchemaName = [S].[name]
+					  , DBName = @DB
+					  , [ObjectDefinition]=null --Object_Definition([T].[object_id])
+				From    [sys].[tables] As [T]
+						Left Join [sys].[schemas] As [S] On [S].[schema_id] = [T].[schema_id]
+				where Lower([T].[name]) Like ''' + @ObjectName + '''
+			end';
+
+                Exec [Process].[ExecForEachDB] @cmd = @SQLScript
+            End;
+        If @TableView In ( 'view' , 'both' )
+            Begin
+                Set @SQLScript = 'Use [?]; 
+			Declare @DB Varchar(250)= Db_Name();
+			if @DB not in(''tempdb'',''master'',''model'',''msdb'') and lower(@DB) like '''
+                    + @DBToSearch
+                    + '''
+			begin
+				Insert [#ObjectList]
+						( [object_id]
+						, [ObjectName]
+						, [ObjectType]
+						, [SchemaName]
+						, [DBName]
+						, [ObjectDefinition]
+						)
+				Select  [V].[object_id]
+					  , ObjectName = [V].[name]
+					  , ObjectType = ''View''
+					  , SchemaName = [S].[name]
+					  , DBName = @DB
+					  , Object_Definition([V].[object_id])
+				From    [sys].[views]  As [V]
+						Left Join [sys].[schemas] As [S] On [S].[schema_id] = [V].[schema_id]
+				where Lower([V].[name]) Like ''' + @ObjectName + ''';
+			End';
 
                 Exec [Process].[ExecForEachDB] @cmd = @SQLScript;
 
             End;
-        If @TableView In ( 'View' , 'Both' )
-            Begin
-                Set @SQLScript = 'Use [?]; 
-			Declare @DB Varchar(250)= Db_Name();
-			Insert [#ObjectList]
-					( [object_id]
-					, [ObjectName]
-					, [ObjectType]
-					, [SchemaName]
-					, [DBName]
-					, [ObjectDefinition]
-					)
-			Select  [V].[object_id]
-				  , ObjectName = [V].[name]
-				  , ObjectType = ''View''
-				  , SchemaName = [S].[name]
-				  , DBName = @DB
-				  , Object_Definition([V].[object_id])
-			From    [sys].[views]  As [V]
-					Left Join [sys].[schemas] As [S] On [S].[schema_id] = [V].[schema_id];';
 
-                Exec [Process].[ExecForEachDB] @cmd = @SQLScript;
-
-            End;
-
---get list of columns
+--get list of columns that have a collation (so are text) and have a length that will fit the search string
         Set @SQLScript = 'Use [?]; 
 	Declare @DB Varchar(250)= Db_Name();
-	if @DB not in(''tempdb'',''master'',''model'',''msdb'')
+	if @DB not in(''tempdb'',''master'',''model'',''msdb'') and lower(@DB) like '''
+            + @DBToSearch + '''
 	begin
 		Insert [#ColumnList]
 				( [object_id]
@@ -111,32 +126,19 @@ As
 				, [max_length]
 				, [DBName]
 				)
-		SELECT [C].[object_id]
-				,[C].[column_id]
-				,[C].[name]
-				,[C].[system_type_id]
-				,[max_length]
-				,@DB
-		 FROM sys.[columns] As [C];
+		SELECT    [C].[object_id]
+				, [C].[column_id]
+				, [C].[name]
+				, [C].[system_type_id]
+				, [max_length]
+				, @DB
+		 FROM sys.[columns] As [C]
+		 where collation_name is not null
+		 and [max_length]>=(' + Convert(Varchar(10) , Len(@TextToSearch))
+            + '-2);
 	end';
+
         Exec [sys].[sp_MSforeachdb] @SQLScript;
-
---Remove fields that are too small, not in the right db or schema and are not a text type field
-        Delete  [#ColumnList]
-        From    [#ColumnList] As [C]
-                Left Join [#ObjectList] As [OL] On [OL].[object_id] = [C].[object_id]
-                Left Join [sys].[types] As [T] On [C].[ColumnType] = [T].[system_type_id]
-        Where   [C].[max_length] < Len(@TextToSearch)
-                Or Upper([OL].[DBName]) Not Like Upper(@DBToSearch)
-                Or Upper([OL].[ObjectName]) Not Like Upper(@ObjectName)
-                Or Upper([OL].[SchemaName]) Not Like Upper(@SchemaToSearch)
-                Or [T].[name] Not In ( 'text' , 'ntext' , 'varchar' , 'char' ,
-                                       'nvarchar' , 'nchar' , 'xml' ,
-                                       'sysname' )
-                Or [OL].[object_id] Is Null;
-
-        /*Alter Table [#ColumnList] 
-        Add [ExecID] Int Identity(1,1);*/
 
         Declare @CurrentID Int
           , @MaxID Int
@@ -203,15 +205,17 @@ As
                         + QuoteName([OL].[SchemaName]) + '.'
                         + QuoteName([OL].[ObjectName]) + '
 						where lower(' + QuoteName([CL].[ColumnName])
-                        + ') collate Latin1_General_CI_AI like lower('''
-                        + @TextToSearch + ''') collate Latin1_General_CI_AI
+                        + ') collate Latin1_General_CI_AI like '''
+                        + @TextToSearch + ''' collate Latin1_General_CI_AI
 						having count(1)>0'
                 From    [#ColumnList] As [CL]
-                        Left Join [#ObjectList] As [OL] On [OL].[object_id] = [CL].[object_id]
-                                                           And [OL].[DBName] = [CL].[DBName]
+                        Left Join [#ObjectList] As [OL]
+                            On [OL].[object_id] = [CL].[object_id]
+                               And [OL].[DBName] = [CL].[DBName]
                 Where   [CL].[ExecID] = @CurrentID;		
 		
                 Begin Try
+                    Print @SQLScript;
                     Exec (@SQLScript);
                 End Try
                 Begin Catch
@@ -234,8 +238,9 @@ As
                                           , 'Unable to execute against'
                                           , 'Unable to execute against'
                                     From    [#ColumnList] As [CL]
-                                            Left Join [#ObjectList] As [OL] On [OL].[object_id] = [CL].[object_id]
-                                                              And [OL].[DBName] = [CL].[DBName]
+                                            Left Join [#ObjectList] As [OL]
+                                                On [OL].[object_id] = [CL].[object_id]
+                                                   And [OL].[DBName] = [CL].[DBName]
                                     Where   [CL].[ExecID] = @CurrentID;	
                         End;
                 End Catch;
@@ -270,6 +275,7 @@ As
                 From    [#ColumnList] As [CL];
             End;
 
+        Set NoCount Off;
         Select  [DBName] = QuoteName([R].[DBName])
               , [Dot1] = '.'
               , [SchemaName] = QuoteName([R].[SchemaName])
@@ -279,8 +285,8 @@ As
               , [Script] = 'SELECT * FROM ' + QuoteName([R].[DBName]) + '.'
                 + QuoteName([R].[SchemaName]) + '.'
                 + QuoteName([R].[TableName]) + '
-where ' + QuoteName([R].[ColumnName]) + ' like '''
-                + @TextToSearch + ''''
+where lower(' + QuoteName([R].[ColumnName]) + ') like ''' + @TextToSearch
+                + ''''
               , [R].[CountofMatchingRows]
               , [R].[MaxMatchingRow]
               , [R].[MinMatchingRow]
